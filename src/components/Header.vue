45df276eb009954325465ebd4c2179414b6908f0
Constantin Radu <radu17constantin@gmail.com>
2019-09-16 23:41:38 +0300

    Clean project & add Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="header">
+    <span>This is the header Component...</span>
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Header extends Vue {}
+</script>
+
+<style scoped lang="scss">
+.header {
+  display: flex;
+  flex-flow: row nowrap;
+}
+</style>
